12c4fdf1e273a37ff7105d5f58ad80363fddadea
JCOlazo1 <jay.olazo@gmail.com>
2020-12-03 18:27:57 -0500

    Initial Commit: useState + APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [joke, setJoke] = useState("");
+  const [jokeId, setJokeId] = useState(0);
+  // Axios retrieving from an API
+  const getJoke = () => {
+    axios.get("https://official-joke-api.appspot.com/random_joke").then((response) => {
+      console.log(response);
+      setJoke(response.data.setup + " ... " + response.data.punchline);
+      setJokeId(response.data.id);
+    })
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="counter-container">
+        Simple Counter: {count}
+
+        <button onClick={() => setCount(currentValue => currentValue + 1)}>+</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+      < hr />
+      <div className="logger-container">
+        User Input:
+        {/**USER EMAIL */}
+        <input
+          name='email'
+          value={email}
+          placeholder='email'
+          onChange={something => setEmail(something.target.value)}
+        />
+        {/**USER PASSWORD */}
+        <input
+          type='password'
+          name='password'
+          placeholder='password'
+          value={password}
+          onChange={something => setPassword(something.target.value)} />
+      </div>
+      <hr />
+      <div className="api-container">
+        <button onClick={getJoke}>Click for a joke</button>
+        {joke}
+        <br />
+        This is joke number {jokeId}
+      </div>
+    </>
   );
 }
 
